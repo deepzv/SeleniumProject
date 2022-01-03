830a377597efe4ddd511ff30a6f5c8e499eaaa60
deepzv <deepzvij303@gmail.com>
2022-01-03 22:27:05 +0530

    initial

---
+++ src/subStringdemo.java
@@ -0,0 +1,17 @@
+public class subStringdemo {
+
+    public static void main(String[] args)
+    {
+        String str = "HELLO";
+
+        /*System.out.println(str.substring(0)+str.substring(0,0));
+        System.out.println(str.substring(1)+str.substring(0,1));
+        System.out.println(str.substring(2)+str.substring(0,2));
+        System.out.println(str.substring(3)+str.substring(0,3));
+        System.out.println(str.substring(4)+str.substring(0,4)); */
+
+        for(int i=0;i<str.length();i++)
+            System.out.println(str.substring(i)+str.substring(0,i));
+    }
+}
+
